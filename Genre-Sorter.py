bf1da39a76ec2e81118a48bf973025f7c58f9ee7
shanewazhere <shanesalisburywazhere@gmail.com>
2015-11-07 02:54:52 -0700

    Third commit

--- Genre-Sorter.py
+++ Genre-Sorter.py
@@ -1,42 +1,48 @@
-import pygame, sys, time
-from pygame.locals import *
+import Tkinter,sys, time
+from Tkinter import *
+from tkFileDialog import askopenfilename
 
-pygame.init()
-fpsClock = pygame.time.Clock()
-
-windowSurfaceObj = pygame.display.set_mode((640, 480))
-pygame.display.set_caption('Genre Sorter')
-
-mousex, mousey = 0, 0
-msg = 0
-
-while True:
 	
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
-		elif event.type == MOUSEMOTION:
-			mousex, mousey = event.pos
-		elif event.type == MOUSEBUTTONUP:
-			mousex, mousey = event.pos
-			if event.button in (1, 2, 3):
-				msg = 'left, middle, or right mouse click'
-			elif event.button in (4, 5):
-				msg = 'mouse scrolled up or down'
-				
-		elif event.type == KEYDOWN:
-			if event.key in (K_LEFT, K_RIGHT, K_UP, K_DOWN):
-				msg = 'Arrow key pressed'
-			if event.key == K_a:
-				msg = '"A" key pressed'
-			if event.key == K_ESCAPE:
-				pygame.event.post(pygame.event.Event(QUIT))
-			
-				
-			
-			
-		print msg
-	pygame.display.update()
-	fpsClock.tick(30)
+def openfile():
+	
+	filename = askopenfilename(parent=window)
+	f = open(filename)
+	f.read()
+	f.close()
+
+def song():
+	tkMessageBox.showinfo("Reset", "...Opened Successfully!")	
+	A = Tkinter.Button(window, text ="Analyze Song", fg="green", command = analyzed)
+	A.pack()
+	
+def msgbox():
+	openfile()
+	song()
+
+def analyzed():
+	tkMessageBox.showinfo("Analyze Song", "...Analyzed successfully!")
+	tkMessageBox.showinfo("Genre determined", "Determined song genre according to analysis: ")
+	tkMessageBox.showinfo("Correct", "Is this the correct genre?")
+	Y = Tkinter.Button(window, text ="Yes")
+	N = Tkinter.Button(window, text="No")
+	Y.pack()
+	N.pack()
+	
+import tkMessageBox
+window = Tk()
+
+B = Tkinter.Button(window, text ="Reset", fg="red")
+B.pack()
+
+S = Tkinter.Button(window, text ="Open Song", fg="blue", command = msgbox)
+S.pack()
+
+
+
+window.title("Genre-Sorter")
+window.geometry("500x500")
+window.mainloop()
+
+        
+
 
